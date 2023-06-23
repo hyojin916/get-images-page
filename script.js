b46798535cd5d027a2f1d3c55343eb7d5cb10344
hyojin <gywlsh274@gmail.com>
2023-06-23 16:20:41 +0900

    이미지 랜덤 생성 불러오기 완료

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const generateBtn = document.querySelector('#generateBtn');
+const gridContainer = document.querySelector('#gridContainer');
+
+generateBtn.addEventListener('click', function () {
+  const img = document.createElement('img');
+  const randomId = Math.floor(Math.random() * 1000) + 1;
+
+  img.src = `https://picsum.photos/id/${randomId}/150/150?random=${randomId}`;
+  // 버튼이 클릭되었을 때 실행되는 코드
+  gridContainer.appendChild(img);
+});
